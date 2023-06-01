492e93bd7045288a88e112fa263010b63cfedb0e
Rogier Velting <rogier@codegorilla.nl>
2023-06-01 11:44:42 +0200

    thank you git

---
+++ set-env.ts
@@ -0,0 +1,24 @@
+// import { writeFile } from 'fs';
+const writeFile = require('fs').writeFile;
+
+// Configure Angular `environment.ts` file path
+const targetPath = './src/environments/environment.ts';
+
+// Load node modules
+const colors = require('colors');
+require('dotenv').load();
+
+// `environment.ts` file structure
+const envConfigFile = `export const environment = {
+   url: '${process.env['API_URL']}'
+};
+`;
+console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+console.log(colors.grey(envConfigFile));
+writeFile(targetPath, envConfigFile, function (err: any) {
+   if (err) {
+       throw console.error(err);
+   } else {
+       console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+   }
+});
